1816bec249c1cdd39d432482eb95d140ce688188
Ross Patterson <me@rpatterson.net>
2013-08-22 07:42:16 -0700

    Arbitrary headers

--- doc/norg-demo.js
+++ doc/norg-demo.js
@@ -1,5 +1,12 @@
 angular.module('nOrg', []).
     controller('NOrgCtrl', function($scope) {
+        var reserved_headers = {"Subject": true, "Message-ID": true};
+        $scope.filter_headers = function(node) {
+            var keys = [];
+            for (var key in node.headers) {
+                if (!(key in reserved_headers)) {
+                    keys.push(key); }; };
+            return keys;};
         $scope.projects = [
             {"id": "/foo.nod",
              "headers": {"Subject": "Foo Project",
@@ -7,11 +14,13 @@ angular.module('nOrg', []).
             },
             {"id": "/bar/.nod",
              "headers": {"Subject": "Bar Project",
-                         "Message-ID": "<2@foo.com>"},
+                         "Message-ID": "<2@foo.com>",
+                         "Bar-Property": "Bar Property"},
              "children": [
                  {"id": "/bar/corge.nod",
                   "headers": {"Subject": "Corge Node",
-                              "Message-ID": "<3@foo.com>"}},
+                              "Message-ID": "<3@foo.com>",
+                              "Corge-Property": "Corge Property"}},
                  {"id": "/bar/grault.nod",
                   "headers": {"Subject": "Grault Node",
                               "Message-ID": "<4@foo.com>"}},
