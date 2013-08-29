676c9b1beac79b24db97c59dba801b63f958ee39
Ross Patterson <me@rpatterson.net>
2013-08-29 03:50:17 -0700

    Add some missing comments

--- src/app/norg.js
+++ src/app/norg.js
@@ -4,11 +4,17 @@ angular.module('nOrg', [
   .controller('NOrgCtrl', function NOrgCtrl ($scope, $http) {
     var reserved_headers = {"Subject": true, "Message-ID": true};
     $scope.listChildren = function (parent) {
+      // Generate a filter function closure with access to the parent
+      // reference
       if (arguments.length === 0) {
         parent = $scope;
       }
       var previous;
       return function (node) {
+        // Process nodes as they are rendered adding utility
+        // attributes deduced from the raw JSON.
+
+        // Parent/Children processing
         node.parent = parent;
         node.children = node.children || [];
         node.previous = previous;
@@ -17,8 +23,11 @@ angular.module('nOrg', [
           node.classes.push('demotable');
         }
 
+        // Generate a valid HTML ID and CSS selector from the message
+        // ID using base64 encoding
         node.id = window.btoa(node.headers["Message-ID"]).slice(0, -1);
 
+        // A list of header names that the UI will care about
         node.header_keys = [];
         for (var key in node.headers) {
           if (!(key in reserved_headers)) {
@@ -30,10 +39,12 @@ angular.module('nOrg', [
       };
     };
     $http.get('app/nodes.json').success(function (data) {
+      // Load the initial nodes JSON
       $scope.children = data;
     });
 
     $scope.demote = function (node) {
+      // Demote a node if appropriate
       if (typeof node.previous == "undefined") {
         throw new Error("Cannot promote first sibling!");
       }
