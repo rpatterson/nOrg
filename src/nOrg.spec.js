072748a242f6ce2a40e973ca9fd37cce8bc071cb
Ross Patterson <me@rpatterson.net>
2013-09-04 01:38:02 -0700

    Node and header inheritance tests

--- src/nOrg.spec.js
+++ src/nOrg.spec.js
@@ -15,6 +15,24 @@ describe('nOrg', function() {
       expect(node).toBeTruthy();
       expect(node.parent).toBe(nOrg.root);
     });
+    it('inherits attrs and headers from parent', function () {
+      var child = node.newChild();
+      node.foo = 'foo';
+      node.headers['Foo-Header'] = 'foo';
+
+      expect(child.foo).toBe('foo');
+      expect(child.headers['Foo-Header']).toBe('foo');
+    });
+    it('child may override parent attrs and headers', function () {
+      var child = node.newChild();
+      node.foo = 'foo';
+      node.headers['Foo-Header'] = 'foo';
+      child.foo = 'bar';
+      child.headers['Foo-Header'] = 'bar';
+
+      expect(child.foo).toBe('bar');
+      expect(child.headers['Foo-Header']).toBe('bar');
+    });
   });
   
 });
