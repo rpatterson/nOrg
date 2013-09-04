b83afc05cbd5309620b12582c09975314eba9f01
Ross Patterson <me@rpatterson.net>
2013-09-04 02:15:43 -0700

    Fix object "class" names in debugger

--- src/nOrg.js
+++ src/nOrg.js
@@ -1,35 +1,19 @@
 var nOrg = (function nOrg() {
   var root;
 
-  // prototypical inheritance from parent nodes/headers
-  function Parented() {
-  }
-  Parented.prototype = {
-    newChild: function newChild(parent) {
-      var Child;
-      var child;
-
-      Child = function ChildNode() {
-      };
-      Child.prototype = parent;
-      child = new Child();
-      if (child.init) {
-        child.init();
-      }
-      child.parent = parent;
-      return child;
-    }
-  };
-
   function Node() {
     this.init();
   }
-  Node.prototype = new Parented();
   Node.prototype.init =  function init() {
     this.headers = new Headers(this);
   };
   Node.prototype.newChild =  function newChild() {
-    var child = Parented.prototype.newChild(this);
+    // prototypical inheritance from parent nodes
+    var child;
+    function Node() {}
+    Node.prototype = this;
+    child = new Node();
+    child.parent = this;
     child.headers = this.headers.newChild();
     child.headers.node = this;
     return child;
@@ -38,12 +22,15 @@ var nOrg = (function nOrg() {
   function Headers(node) {
     this.init(node);
   }
-  Headers.prototype = new Parented();
   Headers.prototype.init =  function init(node) {
     this.node = node;
   };
   Headers.prototype.newChild =  function newChild(node) {
-    var child = Parented.prototype.newChild(this);
+    // prototypical inheritance from parent headerss
+    var child;
+    function Headers() {}
+    Headers.prototype = this;
+    child = new Headers();
     child.node = node;
     return child;
   };
