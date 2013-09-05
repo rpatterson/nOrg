ee297a7147c8f553f132c4a4b1198e4ddf14c2ab
Ross Patterson <me@rpatterson.net>
2013-09-05 09:41:20 -0700

    Integrate test runner into demo

--- src/nOrg.spec.js
+++ src/nOrg.spec.js
@@ -397,3 +397,58 @@ describe('nOrg', function() {
   });
 });
 
+/**
+ ## The Runner and Reporter
+
+ Jasmine is built in JavaScript and must be included into a JS environment, such as a web page, in order to run. Like this web page.
+
+ This file is written in JavaScript and is compiled into HTML via [Rocco][rocco]. The JavaScript file is then included, via a `<script>` tag, so that all of the above specs are evaluated and recorded with Jasmine. Thus Jasmine can run all of these specs. This page is then considered a 'runner.'
+
+ Scroll down the page to see the results of the above specs. All of the specs should pass.
+
+ Meanwhile, here is how a runner works to execute a Jasmine suite.
+
+ [rocco]: http://rtomayko.github.com/rocco/
+ */
+(function() {
+  if (! jasmine.HtmlReporter) {
+    // Running under Karma
+    return;
+  }
+
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 250;
+
+  /**
+   Create the `HTMLReporter`, which Jasmine calls to provide results of each spec and each suite. The Reporter is responsible for presenting results to the user.
+   */
+  var htmlReporter = new jasmine.HtmlReporter();
+  jasmineEnv.addReporter(htmlReporter);
+
+  /**
+   Delegate filtering of specs to the reporter. Allows for clicking on single suites or specs in the results to only run a subset of the suite.
+   */
+  jasmineEnv.specFilter = function(spec) {
+    return htmlReporter.specFilter(spec);
+  };
+
+  /**
+   Run all of the tests when the page finishes loading - and make sure to run any previous `onload` handler
+
+   ### Test Results
+
+   Scroll down to see the results of all of these specs.
+   */
+  var currentWindowOnload = window.onload;
+  window.onload = function() {
+    if (currentWindowOnload) {
+      currentWindowOnload();
+    }
+
+    execJasmine();
+  };
+
+  function execJasmine() {
+    jasmineEnv.execute();
+  }
+})();
