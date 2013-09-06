2c3be6594c22a2a8dbf41945212c6a53516c747d
Ross Patterson <me@rpatterson.net>
2013-09-05 17:46:39 -0700

    Test improvements, clearer intentions better isolation

--- src/nOrg.spec.js
+++ src/nOrg.spec.js
@@ -7,7 +7,10 @@ var expect = expect;
 var nOrg = nOrg;
 describe('nOrg', function() {
   var node;
-  var json = {
+  var json;
+
+  beforeEach(function () {
+    json = {
       "children": [
         {"path": "/foo.nod",
          "headers": {"Subject": "Foo Subject",
@@ -33,15 +36,13 @@ describe('nOrg', function() {
          "headers": {"Subject": "Qux Subject",
                      "Message-ID": "<6@foo.com>"}}
       ]};
-
-  beforeEach(function () {
     nOrg.root = nOrg.newRoot(json);
     node = nOrg.root.$childHead.$nextSibling;
   });
 
   it('exports module contents', function () {
     expect(nOrg.Node).toBeTruthy();
-    expect(nOrg.root).toBeTruthy();
+    expect(Boolean(nOrg.root)).toBeTruthy();
   });
 
   describe('node headers:', function () {
@@ -56,21 +57,22 @@ describe('nOrg', function() {
 
   describe('node inheritance:', function () {
     it('has a parent', function () {
-      expect(node).toBeTruthy();
+      expect(Boolean(node)).toBeTruthy();
       expect(node.$parent).toBe(nOrg.root);
     });
     it('inherits attrs and headers from parent', function () {
       var child = node.newChild();
-      expect(child.path).toBe('/bar/.nod');
-      expect(child.headers['Subject']).toBe("Bar Subject");
+      expect(child.path).toBe(json.children[1].path);
+      expect(child.headers['Subject'])
+        .toBe(json.children[1].headers['Subject']);
     });
     it('child may override parent attrs and headers', function () {
       var child = node.newChild();
-      child.path = 'bar';
-      child.headers['Subject'] = 'Bar Subject';
+      child.path = 'baz';
+      child.headers['Subject'] = 'Baz Subject';
 
-      expect(child.path).toBe('bar');
-      expect(child.headers['Subject']).toBe('Bar Subject');
+      expect(child.path).toBe('baz');
+      expect(child.headers['Subject']).toBe('Baz Subject');
     });
     it('can inherit root node', function () {
       expect(node.hasOwnProperty("$root")).toBeFalsy();
@@ -154,14 +156,14 @@ describe('nOrg', function() {
       var child = node.newChild();
 
       child.extend(object);
-      expect(child.path).toBe('bar');
-      expect(child.headers['Subject']).toBe("Bar Subject");
+      expect(child.path).toBe(object.path);
+      expect(child.headers['Subject']).toBe(object.headers['Subject']);
 
       object.path = 'qux';
       object.headers['Subject'] = "Qux Subject";
       child = node.newChild(object);
-      expect(child.path).toBe('qux');
-      expect(child.headers['Subject']).toBe("Qux Subject");
+      expect(child.path).toBe(object.path);
+      expect(child.headers['Subject']).toBe(object.headers['Subject']);
     });
     it('object children are converted to nodes', function () {
       var object = {"path": "bar",
