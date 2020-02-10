15a37e8511fd07aac7efa15813a878c046bb396f
Ross Patterson <me@rpatterson.net>
2020-02-09 17:51:54 -0800

    [refactor] Cleanup more unused boilerplate

--- components/norg-app/src/NorgApp.js
+++ components/norg-app/src/NorgApp.js
@@ -2,12 +2,6 @@ import { LitElement, html } from 'lit-element';
 
 
 export class NorgApp extends LitElement {
-  static get properties() {
-    return {
-      title: { type: String },
-    };
-  }
-
   render() {
     return html`
       <main>
